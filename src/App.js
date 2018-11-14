e7f5c1c5ebf50c7628c4eb3d4dada6d1bee279b7
ugbechike <engr.ugbechike@gmail.com>
2018-11-14 21:07:10 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {Route, Switch} from 'react-router-dom';
+// import './App.css';
+import Styles from '../src/components/style/styles'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <Switch>
+          <Route path='/style' component={Styles} exact/>
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/style/styles.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from '../header/header';
+
+export default class Styles extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    )
+  }
+}
